22ac1dab6d4f7de89016565e99a5e032804e3df4
Ankit Nakarmi <ankit.nakarmi23@gmail.com>
2020-08-26 19:47:35 +0545

    create simple tab

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [activetab, setTab] = useState(0);
+
+  function handleTabChange(index) {
+    setTab(index);
+  }
+
+  // function getForm() {
+  //   if (activetab === 0) {
+  //     return (
+  //       <form name='login'>
+  //         <input type='text' placeholder='Username' />
+  //         <input type='password' placeholder='password' />
+  //       </form>
+  //     );
+  //   }
+  //   if (activetab === 1) {
+  //     return (
+  //       <form name='register'>
+  //         <input type='text' placeholder='Username' />
+  //         <input type='email' placeholder='Email' />
+  //         <input type='password' placeholder='password' />
+  //       </form>
+  //     );
+  //   }
+  //   return null;
+  // }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick={(event) => handleTabChange(0)}>Login</button>
+      <button onClick={(event) => handleTabChange(1)}>Register</button>
+      {activetab === 0 ? (
+        <form name='login'>
+          <input type='text' placeholder='Username' />
+          <input type='password' placeholder='password' />
+        </form>
+      ) : (
+        <form name='register'>
+          <input type='text' placeholder='Username' />
+          <input type='email' placeholder='Email' />
+          <input type='password' placeholder='password' />
+        </form>
+      )}
+
+      {/* {getForm()} */}
     </div>
   );
 }
